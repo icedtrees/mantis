01332c30c4d4759cf90dfa7e5ab52e0d54eee0a1
Leo Huang <crazysheep2302@gmail.com>
2014-11-12 01:23:49 +1100

    Delete build and devel folders from workspace

--- workspace/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/icedtrees/mantis/workspace/src
-# Build directory: /home/icedtrees/mantis/workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(mantis)
--- workspace/build/mantis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/icedtrees/mantis/workspace/src/mantis
-# Build directory: /home/icedtrees/mantis/workspace/build/mantis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
